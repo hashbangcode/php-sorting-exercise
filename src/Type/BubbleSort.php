c4fa184039c41f2936832c2ff91a58df929c7492
Philip Norton <philipnorton42@gmail.com>
2023-02-13 21:23:47 +0000

    Adding answers.

--- src/Type/BubbleSort.php
+++ src/Type/BubbleSort.php
@@ -12,6 +12,16 @@ class BubbleSort implements SortInterface
      */
     public function sort(array $array): array
     {
-        // todo: write bubble sort.
+        for ($i = 0; $i < count($array) - 1; $i++) {
+            for ($j = 0; $j < count($array) - 1; $j++) {
+                if ($array[$j] > $array[$j + 1]) {
+                    $x = $array[$j + 1];
+                    $array[$j + 1] = $array[$j];
+                    $array[$j] = $x;
+                }
+            }
+        }
+
+        return $array;
     }
 }
--- src/Type/QuickSort.php
+++ src/Type/QuickSort.php
@@ -12,6 +12,21 @@ class QuickSort implements SortInterface
      */
     public function sort(array $array): array
     {
-        // todo: write quick sort.
+        if (count($array) === 0) {
+            return $array;
+        }
+
+        $k = $array[0];
+        $x = $y = [];
+
+        for ($i = 1; $i < count($array); $i++) {
+            if ($array[$i] <= $k) {
+                $x[] = $array[$i];
+            } else {
+                $y[] = $array[$i];
+            }
+        }
+
+        return array_merge($this->sort($x), array($k), $this->sort($y));
     }
 }
